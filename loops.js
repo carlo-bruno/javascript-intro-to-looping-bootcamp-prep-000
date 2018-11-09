e70abbb04df212bcc63b23c4cae7a04b80f3b51b
Carlo Bruno <carlofbruno@gmail.com>
2018-11-09 07:52:34 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,5 +2,5 @@ function forLoop(array) {
   for (let i=0; i < array.length; i++) {
     let string = `I am ${i} strange loop${i === 0 ? '' : 's'}.`;
   }
-    return array;
+    return array.length;
 }
